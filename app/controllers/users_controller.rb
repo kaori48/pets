2770ec947539adad7474d70079250ed03059b53c
kaori48 <kaori2501stargaingalaxy@gmail.com>
2020-03-05 08:26:20 +0000

    [Add]ユーザー編集機能、サイドバー

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,23 @@ class UsersController < ApplicationController
   def index
   end
 
-  def show
-  	#ユーザー情報編集
+  def show#ユーザー情報
+  	@user = User.find(params[:id])
   end
-
+  def update
+  	@user = User.find(params[:id])
+  	@user.update(user_params)
+  	redirect_to user_path
+  end
+  def delete
+  end
   def edit
   end
 
   def mypage
+  end
+  private
+  def user_params
+  params.require(:user).permit(:name, :introduction, :icon)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'fbd18a4818247f407eb1e9d7c0413672aac6b1e8e3ed0532380a9f94828f0fd14a79a5d846caaae3ad492083f968b41fd792bd088738a69c097b523f8406d160'
