0a8a5af727001e32923aa1053780cae221a7971e
kaori48 <kaori2501stargaingalaxy@gmail.com>
2020-03-07 04:34:48 +0000

    [Add]一覧表示、編集、削除、詳細ページ

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'#localhosst3000する
   get 'home/about' => 'homes#about'
-  resources :users
   get 'users/mypage' => 'users#mypage'
+  resources :users
   resources :blogs do
   	resource :blog_comments, only: [:create, :destroy, :update, :edit]
   end
