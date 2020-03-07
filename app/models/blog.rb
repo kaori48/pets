e0dbc0448dd7cafc22b8c1b1f751478f7c12aecd
kaori48 <kaori2501stargaingalaxy@gmail.com>
2020-03-07 02:48:36 +0000

    [Add]ジャンル選択機能

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,7 @@
 class Blog < ApplicationRecord
-	belongs_to :user,optional: true #関連先を検査しないようにする
+	belongs_to :user
+	has_many :blog_genre_relations
+	has_many :genres, through: :blog_genre_relations#中間テーブルを経由して関連づけ
 	has_many :blog_comments
 	attachment :blog_image #refile,id入れない
 end
