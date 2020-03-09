af139c79b040f4669d19d56c1d410895ff40a0e1
kaori48 <kaori2501stargaingalaxy@gmail.com>
2020-03-09 01:10:09 +0000

    [Add]テーブル、リレーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_08_124112) do
+ActiveRecord::Schema.define(version: 2020_03_09_010448) do
 
   create_table "animals", force: :cascade do |t|
     t.integer "user_id"
-    t.string "animal_id"
+    t.string "animal_image_id"
     t.string "name"
     t.string "birth"
     t.text "food"
