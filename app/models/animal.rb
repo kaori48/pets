d1f18479e9cef0e433edd0446ad60defef889775
kaori48 <kaori2501stargaingalaxy@gmail.com>
2020-03-10 10:40:09 +0000

    [Add]ペットコメントモデル、コントローラ

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,4 +1,5 @@
 class Animal < ApplicationRecord
 	belongs_to :user
 	attachment :animal_image #refile,id入れない
+	has_many :animal_comments, dependent: :destroy#ペットのコメント
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_09_070429) do
+ActiveRecord::Schema.define(version: 2020_03_10_103033) do
+
+  create_table "animal_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "animal_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "animals", force: :cascade do |t|
     t.integer "user_id"
