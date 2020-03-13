c63cd9acbc15e8986029a2e85de1bb52756ad7f8
kaori48 <kaori2501stargaingalaxy@gmail.com>
2020-03-13 14:53:05 +0000

    [Add]kaminari適用

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,11 +1,13 @@
 class AnimalsController < ApplicationController
+  before_action :authenticate_user!#ログインしていない人をログイン画面へ
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]#本人以外できないようにする
   def index
   end
 
   def show
   	@animal = Animal.find(params[:id])
   	@comment = AnimalComment.new#コメント機能新規
-  	@comments = @animal.animal_comments#コメント一覧
+  	@comments = @animal.animal_comments.order(created_at: :asc).page(params[:page]).reverse_order#コメント一覧
     #サイドバー
     @user = current_user
     @applying = AnimalPermit.find_by(permitter_id: current_user.id, permitted_id: @user.id )
@@ -44,6 +46,14 @@ class AnimalsController < ApplicationController
   	animal = Animal.find(params[:id])
   	animal.destroy
   	redirect_to user_animal_path(current_user.id)#userのanimalページに飛ぶ
+  end
+
+  #編集制限
+  def ensure_correct_user
+    @blog = Blog.find(params[:id])
+    if @blog.user != current_user
+      redirect_to action: :index#一覧へ戻す
+    end
   end
   private
   def animal_params
--- app/models/animal.rb
+++ app/models/animal.rb
@@ -2,4 +2,5 @@ class Animal < ApplicationRecord
 	belongs_to :user
 	attachment :animal_image #refile,id入れない
 	has_many :animal_comments, dependent: :destroy#ペットのコメント
+	validates :name, presence: true #空欄NG
 end
