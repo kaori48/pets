ddfa77ae50d1383730fbf8e1bc30c4eee92ef22c
kaori48 <kaori2501stargaingalaxy@gmail.com>
2020-03-15 10:46:15 +0000

    [Add]コメントエラー

--- app/controllers/blog_comments_controller.rb
+++ app/controllers/blog_comments_controller.rb
@@ -3,8 +3,12 @@ class BlogCommentsController < ApplicationController
 		@blog = Blog.find(params[:blog_id])#blogのidをとってくる
 		@comment = @blog.blog_comments.new(blog_comment_params)#
 		@comment.user_id = current_user.id #コメントしてる人のユーザーid
-		@comment.save
-		redirect_to request.referer#ひとつ前のURLを返す
+		if @comment.save
+		   redirect_to request.referer#ひとつ前のURLを返す
+		else
+		   flash[:notice] = "コメントを入力してください"#成功メッセ
+  	       redirect_to request.referer#ひとつ前のURLを返す
+  	    end
 	end
 	def destroy
 		@comment = BlogComment.find(params[:blog_id])
