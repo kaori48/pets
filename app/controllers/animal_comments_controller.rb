d2dcd5baaad197fea608adba2a7f6f9674873fb0
kaori48 <kaori2501stargaingalaxy@gmail.com>
2020-03-15 10:40:36 +0000

    [Add]コメントエラー

--- app/controllers/animal_comments_controller.rb
+++ app/controllers/animal_comments_controller.rb
@@ -1,10 +1,15 @@
 class AnimalCommentsController < ApplicationController
 	def create
-		animal = Animal.find(params[:animal_id])#animalのidをとってくる
-		comment = animal.animal_comments.new(animal_comment_params)
-		comment.user_id = current_user.id
-		comment.save
-		redirect_to request.referer#ひとつ前のURLを返す
+		@animal = Animal.find(params[:animal_id])#animalのidをとってくる
+		@comment = @animal.animal_comments.new(animal_comment_params)
+		@comment.user_id = current_user.id
+		if @comment.save
+			flash[:notice] = "投稿しました！"#成功メッセ
+		    redirect_to request.referer#ひとつ前のURLを返す
+		else
+  	        flash[:notice] = "コメントを入力してください"#成功メッセ
+  	        redirect_to request.referer#ひとつ前のURLを返す
+        end
 	end
 
 	def destroy
