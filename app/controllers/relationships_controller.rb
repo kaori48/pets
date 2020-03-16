9e5bf20c5354fda555127d200d74683eca504d67
kaori48 <kaori2501stargaingalaxy@gmail.com>
2020-03-16 07:20:25 +0000

    [Add]フォロー、フォロワー一覧

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -8,11 +8,13 @@ class RelationshipsController < ApplicationController
         redirect_to request.referer
 	end
 	def follows
-		user = User.find(params[:user_id])#レコードとってく
-		@users = user.following_user#
+		@user = User.find(params[:id])#レコードとってく
+		@follow_users = Relationship.where(follower_id: @user.id).order(created_at: :asc).page(params[:page]).reverse_order
 	end
 	def followers
-		user = User.find(params[:user_id])#レコードとってく
-		@users = user.follower_user#
+		@applying = AnimalPermit.find_by(permitter_id: current_user.id, permitted_id: @user )#サイドバー
+		@user = User.find(params[:user_id])#レコードとってく
+		@follow_users = Relationship.where(follower_id: @user.id).order(created_at: :asc).page(params[:page]).reverse_order#フォロー一覧
+		@follower_users = Relationship.where(followed_id: @user.id).order(created_at: :asc).page(params[:page]).reverse_order#フォロワー一覧
 	end
 end
